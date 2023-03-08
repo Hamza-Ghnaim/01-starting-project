f01753756379dbfc7938daef674f209065a5bbbb
Hamza <hamzaghnaim99@gmail.com>
2023-03-08 15:51:47 -0500

    transformed into toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,23 +2,34 @@ import classes from './Counter.module.css';
 import { useSelector,useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const counter = useSelector(state=>state.counter);
+  const counter = useSelector((state) => (state.counter));
+  const show = useSelector((state) => (state.showCounter));
+
   const dispatch = useDispatch();
 
   const increase = () => {
     dispatch({type:'increament'});
   };
+  const Increament5 = ()=>{
+    dispatch({type:'Increament5',amount:5});
+  }
   const decrease = () => {
     dispatch({type:'decreament'});
   };
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <button onClick={increase}>Increament</button>
-      <button onClick={decrease}>Decreament</button>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={increase}>Increament</button>
+        <button onClick={decrease}>Decreament</button>
+        <button onClick={Increament5}>Increament 5</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,56 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
-const counterReducer = (state = {counter : 0},action)=>{
-    if (action.type === ' increament') {
+
+const initialState = {counter : 0 , showCounter:true};
+createSlice({
+    name:'counter',
+    initialState,
+    reducers:{
+        increament(state){
+            state.counter++;
+        },
+        decreament(state){
+            state.counter--;
+        },
+        Increament5(state,action){
+            state.counter=state.counter+action.amount;
+        },
+        toggle(state){
+            state.showCounter=!state.showCounter;
+        },
+    }
+
+})
+
+const counterReducer = (state = initialState,action)=>{
+    if (action.type === 'increament') {
         return {
-            counter:state.counter+1
+            counter:state.counter+1,
+            showCounter:state.showCounter
         };
     }
-    if (action.type === ' decreament') {
+
+    if (action.type === 'Increament5') {
         return {
-            counter:state.counter+1
+            counter:state.counter +action.amount,
+            showCounter:state.showCounter
         };
     }
+    if (action.type === 'toggle'){
+        return({
+            counter:state.counter,
+            showCounter:!state.showCounter
+        })
+    }
+
+    if (action.type === 'decreament') {
+        return {
+            counter:state.counter-1,
+            showCounter:state.showCounter
+        };
+    }
+
     else return state;
 };
 
